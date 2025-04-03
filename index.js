67334c713f293a2ab34d509bdcbbd9ba13b0662d
viperr <Antonostr2004@gmail.com>
2025-04-03 20:50:34 +0300

    message

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-'use strict'
+"use strict";
+const vectorLeft = document.querySelector(".vector__left");
+const vectorRight = document.querySelector(".vector__right");
+const cards = document.querySelector(".cards");
+const card = document.querySelectorAll(".card");
+const widthAllElements =
+  card[0].clientWidth * card.length + 30 * (card.length - 1);
+let saveTranslate = 0;
+
+console.log(widthAllElements);
+
+vectorLeft.addEventListener("click", function (event) {
+  if (saveTranslate >= 0) {
+    saveTranslate = -widthAllElements + card[0].clientWidth
+    console.log(saveTranslate);
+    cards.style.transform = `translateX(${saveTranslate}px)`
+  } else {
+    saveTranslate = saveTranslate + (card[0].clientWidth + 30);
+    cards.style.transform = `translateX(${saveTranslate}px)`;
+  }
+});
+
+vectorRight.addEventListener("click", function (event) {
+  console.log(saveTranslate);
+  if (!(saveTranslate <= -widthAllElements + (card[0].clientWidth + 30))) {
+    saveTranslate = saveTranslate - (card[0].clientWidth + 30);
+    cards.style.transform = `translateX(${saveTranslate}px)`;
+  } else {
+    saveTranslate = 0;
+    cards.style.transform = `translateX(0)`;
+  }
+});
